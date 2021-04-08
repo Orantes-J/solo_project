6a032c329100f5f3cc4888e850412611395b793c
Juan Orantes <carlos.oran15@hotmail.com>
2021-04-07 19:45:17 -0700

    secret key correction

--- accreations/settings.py
+++ accreations/settings.py
@@ -29,7 +29,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-# SECRET_KEY = '!5ee2#in@cy0_ituz#yqre!bdvuu_()iq!e06fs_ncs!nl+q9g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
